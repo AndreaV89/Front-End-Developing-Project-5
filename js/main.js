3ec67e8ff814831bc46adcc709bddb67bab301f7
AndreaV <a.vannetti08@gmail.com>
2019-12-21 23:17:53 +0100

    m

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+var $links = $('a');
+var $search = $('#input');
+var caption = [];
 
+$links.each(function(){
+    caption.push({
+        element: this,
+        text: this.title.trim().toLowerCase()
+    });
+});
+
+for(var i=0; i < caption.length; i++) {
+    console.log(caption[i])
+}
